bdb4597a1923721905803c7005e121f24da640f5
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2024-06-08 21:29:52 +0530

    Send response

--- app/server.c
+++ app/server.c
@@ -54,7 +54,9 @@ int main() {
     printf("Waiting for a client to connect...\n");
     client_addr_len = sizeof(client_addr);
 
-    accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+    int fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+    char *reply = "HTTP/1.1 200 OK\r\n\r\n";
+    int byte_sent = send(fd, reply, strlen(reply), 0);
     printf("Client connected\n");
 
     close(server_fd);
