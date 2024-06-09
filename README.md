f98c82ea17d9aec3dd144a96808939fae7da07d1
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2024-06-09 23:32:59 +0530

    Refactored code and modified Readme

--- README.md
+++ README.md
@@ -1,38 +1,76 @@
-[![progress-banner](https://backend.codecrafters.io/progress/http-server/3addea1b-6ee8-42f8-b9b4-8754c93034c7)](https://app.codecrafters.io/users/codecrafters-bot?r=2qF)
+# Simple HTTP Server
 
-This is a starting point for C solutions to the
-["Build Your Own HTTP server" Challenge](https://app.codecrafters.io/courses/http-server/overview).
+This is a simple HTTP server implemented in C. It supports handling GET and POST requests, serving static files, and performing basic routing based on the requested path.
 
-[HTTP](https://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol) is the
-protocol that powers the web. In this challenge, you'll build a HTTP/1.1 server
-that is capable of serving multiple clients.
+## Features
 
-Along the way you'll learn about TCP servers,
-[HTTP request syntax](https://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html),
-and more.
+- Handles GET and POST requests
+- Serves static files from a specified directory
+- Supports routing based on the requested path
+- Echoes back the requested path for the `/echo` endpoint
+- Returns the user agent string for the `/user-agent` endpoint
+- Supports concurrent request handling using threads
 
-**Note**: If you're viewing this repo on GitHub, head over to
-[codecrafters.io](https://codecrafters.io) to try the challenge.
+## Getting Started
 
-# Passing the first stage
+### Prerequisites
 
-The entry point for your HTTP server implementation is in `app/server.c`. Study
-and uncomment the relevant code, and push your changes to pass the first stage:
+- C compiler (e.g., GCC)
+- POSIX-compliant operating system (e.g., Linux, macOS)
 
-```sh
-git add .
-git commit -m "pass 1st stage" # any msg
-git push origin master
-```
+### Building the Server
 
-Time to move on to the next stage!
+1. Clone the repository:
 
-# Stage 2 & beyond
+  git clone https://github.com/RakeshKumar132457/http-server-c
 
-Note: This section is for stages 2 and beyond.
+2. Navigate to the project directory:
 
-1. Ensure you have `gcc` installed locally
-1. Run `./your_server.sh` to run your program, which is implemented in
-   `app/server.c`.
-1. Commit your changes and run `git push origin master` to submit your solution
-   to CodeCrafters. Test output will be streamed to your terminal.
+  cd simple-http-server
+
+3. Compile the server:
+
+  gcc -o server server.c -lpthread
+
+### Running the Server
+
+To start the server, run the following command:
+
+./server -d /path/to/directory
+
+Replace `/path/to/directory` with the directory path where the server should look for static files.
+
+The server will start listening on port 4221 by default.
+
+## Usage
+
+Once the server is running, you can send HTTP requests to it using tools like `curl` or a web browser.
+
+### Endpoints
+
+- `/`: Returns a plain text response with an empty body.
+- `/echo/<message>`: Echoes back the `<message>` in the response body.
+- `/user-agent`: Returns the user agent string of the client making the request.
+- `/files/<filename>`: Serves the specified file from the directory provided during server startup.
+
+### Examples
+
+- Get the user agent string:
+
+ curl http://localhost:4221/user-agent
+
+- Echo a message:
+
+ curl http://localhost:4221/echo/Hello,%20World!
+
+- Serve a static file:
+
+ curl http://localhost:4221/files/example.txt
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- app/server.c
+++ app/server.c
@@ -11,18 +11,284 @@
 
 #define MAX_BUFFER_SIZE 4096
 #define MAX_HEADER_SIZE 1024
+#define MAX_HEADERS 20
 #define MAX_FILENAME_LENGTH 256
 #define SERVER_PORT 4221
 #define BACKLOG 1
 
-char *dir_path = NULL;
+char *dir_path = "/tmp";
+
+typedef enum {
+    HTTP_OK = 200,
+    HTTP_CREATED = 201,
+    HTTP_BAD_REQUEST = 400,
+    HTTP_NOT_FOUND = 404,
+    HTTP_INTERNAL_SERVER_ERROR = 500,
+} HttpStatusCode;
+
+typedef enum {
+    HTTP_GET,
+    HTTP_POST,
+} HttpMethod;
 
 typedef struct {
-    int status_code;
+    char *key;
+    char *value;
+} Header;
+
+typedef struct {
+    HttpStatusCode status_code;
     const char *reason_phrase;
     const char *content_type;
+    Header headers[MAX_HEADERS];
+    int num_headers;
     char *body;
 } Response;
+
+typedef struct {
+    HttpMethod method;
+    const char *path;
+    Response *(*handler)(const char *, const char *);
+} Route;
+
+Response *create_response(HttpStatusCode status_code, const char *content_type, const char *body) {
+    Response *response = (Response *)malloc(sizeof(Response));
+    response->status_code = status_code;
+    response->reason_phrase = NULL;
+    response->content_type = content_type;
+    response->num_headers = 0;
+    response->body = strdup(body);
+    return response;
+}
+
+void set_header(Response *response, const char *key, const char *value) {
+    if (response->num_headers < MAX_HEADERS) {
+        response->headers[response->num_headers].key = strdup(key);
+        response->headers[response->num_headers].value = strdup(value);
+        response->num_headers++;
+    }
+}
+
+char *serialize_status_line(Response *response) {
+    const char *reason_phrase;
+    switch (response->status_code) {
+    case HTTP_OK:
+        reason_phrase = "OK";
+        break;
+    case HTTP_CREATED:
+        reason_phrase = "Created";
+        break;
+    case HTTP_BAD_REQUEST:
+        reason_phrase = "Bad Request";
+        break;
+    case HTTP_NOT_FOUND:
+        reason_phrase = "Not Found";
+        break;
+    case HTTP_INTERNAL_SERVER_ERROR:
+        reason_phrase = "Internal Server Error";
+        break;
+    default:
+        reason_phrase = "Unknown";
+        break;
+    }
+
+    char *status_line = (char *)malloc(MAX_HEADER_SIZE);
+    snprintf(status_line, MAX_HEADER_SIZE, "HTTP/1.1 %d %s\r\n", response->status_code, reason_phrase);
+    return status_line;
+}
+
+char *serialize_headers(Response *response) {
+    char *headers = (char *)malloc(MAX_HEADER_SIZE);
+    strcpy(headers, "");
+
+    char header[MAX_HEADER_SIZE];
+    snprintf(header, MAX_HEADER_SIZE, "Content-Type: %s\r\n", response->content_type);
+    strcat(headers, header);
+
+    for (int i = 0; i < response->num_headers; i++) {
+        snprintf(header, MAX_HEADER_SIZE, "%s: %s\r\n", response->headers[i].key, response->headers[i].value);
+        strcat(headers, header);
+    }
+
+    strcat(headers, "\r\n");
+    return headers;
+}
+
+char *serialize_body(Response *response) {
+    return strdup(response->body);
+}
+
+char *serialize_response(Response *response) {
+    char *status_line = serialize_status_line(response);
+    char *headers = serialize_headers(response);
+    char *body = serialize_body(response);
+
+    int response_length = strlen(status_line) + strlen(headers) + strlen(body);
+    char *response_string = (char *)malloc(response_length + 1);
+    strcpy(response_string, status_line);
+    strcat(response_string, headers);
+    strcat(response_string, body);
+
+    free(status_line);
+    free(headers);
+    free(body);
+
+    return response_string;
+}
+
+void send_response(int client_fd, Response *response) {
+    char *response_str = serialize_response(response);
+    send(client_fd, response_str, strlen(response_str), 0);
+    free(response_str);
+}
+
+void free_response(Response *response) {
+    for (int i = 0; i < response->num_headers; i++) {
+        free(response->headers[i].key);
+        free(response->headers[i].value);
+    }
+    free(response->body);
+    free(response);
+}
+
+char *get_header_value(const char *header_start, const char *header_name) {
+    char *header_end;
+    while ((header_end = strstr(header_start, "\r\n"))) {
+        if (header_start == header_end) {
+            break;
+        }
+
+        char header[MAX_HEADER_SIZE];
+        sscanf(header_start, "%[^\r\n]", header);
+        header_start = header_end + 2;
+
+        char name[MAX_HEADER_SIZE], value[MAX_HEADER_SIZE];
+        if (sscanf(header, "%[^:]: %[^\r\n]", name, value) == 2 && strcmp(name, header_name) == 0) {
+            return strdup(value);
+        }
+    }
+    return NULL;
+}
+
+Response *handle_root(const char *path, const char *request_body) {
+    return create_response(HTTP_OK, "text/plain", "");
+}
+
+Response *handle_echo(const char *path, const char *request_body) {
+    return create_response(HTTP_OK, "text/plain", path + 6);
+}
+
+Response *handle_user_agent(const char *path, const char *request_body) {
+    char *user_agent = get_header_value(request_body, "User-Agent");
+    Response *response = create_response(HTTP_OK, "text/plain", user_agent ? user_agent : "");
+    free(user_agent);
+    return response;
+}
+
+Response *handle_file_get(const char *path, const char *request_body) {
+    char *filename = strstr(path, "/files/");
+    if (filename == NULL) {
+        return create_response(HTTP_NOT_FOUND, "text/plain", "");
+    }
+    filename += strlen("/files/");
+    char file_path[MAX_FILENAME_LENGTH];
+    snprintf(file_path, sizeof(file_path), "%s/%s", dir_path, filename);
+    struct stat buffer;
+    if (stat(file_path, &buffer) == 0) {
+        FILE *file = fopen(file_path, "r");
+        if (file == NULL) {
+            perror("Error opening file\n");
+            return create_response(HTTP_INTERNAL_SERVER_ERROR, "text/plain", "");
+        }
+        char *file_content = malloc(buffer.st_size + 1);
+        if (file_content == NULL) {
+            fclose(file);
+            return create_response(HTTP_INTERNAL_SERVER_ERROR, "text/plain", "");
+        }
+        size_t file_read = fread(file_content, 1, buffer.st_size, file);
+        file_content[file_read] = '\0';
+        fclose(file);
+        Response *response = create_response(HTTP_OK, "application/octet-stream", file_content);
+        free(file_content);
+        return response;
+    } else {
+        return create_response(HTTP_NOT_FOUND, "text/plain", "");
+    }
+}
+
+Response *handle_file_post(const char *path, const char *request_body) {
+    char *filename = strstr(path, "/files/");
+    if (filename == NULL) {
+        return create_response(HTTP_NOT_FOUND, "text/plain", "");
+    }
+    filename += strlen("/files/");
+    char file_path[MAX_FILENAME_LENGTH];
+    snprintf(file_path, sizeof(file_path), "%s/%s", dir_path, filename);
+    printf("%s\n", file_path);
+    if (request_body == NULL) {
+        return create_response(HTTP_BAD_REQUEST, "text/plain", "");
+    }
+
+    FILE *file = fopen(file_path, "w");
+    if (file == NULL) {
+        perror("Error creating file\n");
+        return create_response(HTTP_INTERNAL_SERVER_ERROR, "text/plain", "");
+    }
+    fwrite(request_body, 1, strlen(request_body), file);
+    fclose(file);
+    return create_response(HTTP_CREATED, "text/plain", "");
+}
+
+Route routes[] = {
+    {HTTP_GET, "/echo/", handle_echo},
+    {HTTP_GET, "/user-agent", handle_user_agent},
+    {HTTP_GET, "/files/", handle_file_get},
+    {HTTP_POST, "/files/", handle_file_post},
+    {HTTP_GET, "/", handle_root},
+};
+
+Response *handle_request(const char *request) {
+    char method_str[MAX_BUFFER_SIZE], path[MAX_BUFFER_SIZE], version[MAX_BUFFER_SIZE];
+    sscanf(request, "%s %s %s", method_str, path, version);
+
+    HttpMethod method = HTTP_GET;
+    if (strcmp(method_str, "POST") == 0) {
+        method = HTTP_POST;
+    }
+
+    char *request_body = strstr(request, "\r\n\r\n");
+    if (request_body != NULL) {
+        request_body += strlen("\r\n\r\n");
+    }
+
+    for (int i = 0; i < sizeof(routes) / sizeof(routes[0]); i++) {
+        if (strncmp(path, routes[i].path, strlen(routes[i].path)) == 0 && routes[i].method == method) {
+            return routes[i].handler(path, request_body);
+        }
+    }
+
+    return create_response(HTTP_NOT_FOUND, "text/plain", "");
+}
+
+void *handle_client(void *arg) {
+    int client_fd = *(int *)arg;
+    free(arg);
+
+    char buffer[MAX_BUFFER_SIZE];
+    ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
+    if (bytes_read <= 0) {
+        close(client_fd);
+        return NULL;
+    }
+    buffer[bytes_read] = '\0';
+
+    Response *response = handle_request(buffer);
+    send_response(client_fd, response);
+    free_response(response);
+
+    close(client_fd);
+    return NULL;
+}
 
 int create_server_socket() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -56,168 +322,7 @@ int create_server_socket() {
     return server_fd;
 }
 
-char *get_header_value(const char *header_start, const char *header_name) {
-    char *header_end;
-    while ((header_end = strstr(header_start, "\r\n"))) {
-        if (header_start == header_end) {
-            break;
-        }
-
-        char header[MAX_HEADER_SIZE];
-        sscanf(header_start, "%[^\r\n]", header);
-        header_start = header_end + 2;
-
-        char name[MAX_HEADER_SIZE], value[MAX_HEADER_SIZE];
-        if (sscanf(header, "%[^:]: %[^\r\n]", name, value) == 2 && strcmp(name, header_name) == 0) {
-            return strdup(value);
-        }
-    }
-    return NULL;
-}
-
-Response *create_response(int status_code, const char *reason_phrase, const char *body, const char *content_type) {
-    Response *response = malloc(sizeof(Response));
-    response->status_code = status_code;
-    response->reason_phrase = reason_phrase;
-    response->content_type = content_type;
-    response->body = strdup(body);
-    return response;
-}
-
-Response *process_request(const char *method, const char *path, const char *user_agent, const char *request_body) {
-    if (strcmp(path, "/") == 0) {
-        return create_response(200, "OK", "", "text/plain");
-    } else if (strncmp(path, "/echo/", 6) == 0) {
-        return create_response(200, "OK", path + 6, "text/plain");
-    } else if (strcmp(path, "/user-agent") == 0) {
-        return create_response(200, "OK", user_agent ? user_agent : "", "text/plain");
-    } else if (strstr(path, "/files/") != NULL) {
-        if (strcmp(method, "GET") == 0) {
-            char *filename = strstr(path, "/files/");
-            if (filename == NULL) {
-                return create_response(404, "Not Found", "", "text/plain");
-            }
-            filename += strlen("/files/");
-            char file_path[MAX_FILENAME_LENGTH];
-            snprintf(file_path, sizeof(file_path), "%s/%s", dir_path, filename);
-            struct stat buffer;
-            if (stat(file_path, &buffer) == 0) {
-                FILE *file = fopen(file_path, "r");
-                if (file == NULL) {
-                    perror("Error opening file\n");
-                    exit(1);
-                }
-                char *file_content = malloc(buffer.st_size + 1);
-                if (file_content == NULL) {
-                    fclose(file);
-                    return create_response(500, "Internal Server Error", "", "text/plain");
-                }
-                size_t file_read = fread(file_content, 1, buffer.st_size, file);
-                file_content[file_read] = '\0';
-                fclose(file);
-                Response *response = create_response(200, "OK", file_content, "application/octet-stream");
-                free(file_content);
-                return response;
-            } else {
-                return create_response(404, "Not Found", "", "text/plain");
-            }
-        } else if (strcmp(method, "POST") == 0) {
-            char *filename = strstr(path, "/files/");
-            if (filename == NULL) {
-                return create_response(404, "Not Found", "", "text/plain");
-            }
-            filename += strlen("/files/");
-            char file_path[MAX_FILENAME_LENGTH];
-            snprintf(file_path, sizeof(file_path), "%s/%s", dir_path, filename);
-            printf("%s\n", file_path);
-            if (request_body == NULL) {
-                return create_response(400, "Bad Request", "", "text/plain");
-            }
-
-            FILE *file = fopen(file_path, "w");
-            if (file == NULL) {
-                perror("Error opening file\n");
-                exit(1);
-            }
-            fwrite(request_body, 1, strlen(request_body), file);
-            fclose(file);
-            return create_response(201, "Created", "", "text/plain");
-        }
-    } else {
-        return create_response(404, "Not Found", "", "text/plain");
-    }
-}
-
-void send_response(int fd, Response *response) {
-    char headers[MAX_HEADER_SIZE];
-    snprintf(headers, sizeof(headers),
-             "HTTP/1.1 %d %s\r\n"
-             "Content-Type: %s\r\n"
-             "Content-Length: %zu\r\n"
-             "Connection: keep-alive\r\n"
-             "\r\n",
-             response->status_code, response->reason_phrase, response->content_type, strlen(response->body));
-
-    ssize_t headers_len = strlen(headers);
-    ssize_t body_len = strlen(response->body);
-
-    ssize_t total_len = headers_len + body_len;
-    char *response_buffer = malloc(total_len);
-    if (response_buffer == NULL) {
-        perror("Memory allocation failed");
-        return;
-    }
-
-    memcpy(response_buffer, headers, headers_len);
-    memcpy(response_buffer + headers_len, response->body, body_len);
-
-    ssize_t bytes_sent = send(fd, response_buffer, total_len, 0);
-    if (bytes_sent < 0) {
-        perror("Send failed");
-    }
-
-    free(response_buffer);
-}
-
-void free_response(Response *response) {
-    free(response->body);
-    free(response);
-}
-
-void *handle_request(void *arg) {
-    int client_fd = *(int *)arg;
-    free(arg);
-
-    char buffer[MAX_BUFFER_SIZE];
-    ssize_t byte_read = read(client_fd, buffer, sizeof(buffer) - 1);
-    if (byte_read <= 0) {
-        if (byte_read < 0) {
-            perror("Read failed");
-        }
-        close(client_fd);
-        return NULL;
-    }
-    buffer[byte_read] = '\0';
-    printf("%s\n", buffer);
-
-    char method[MAX_BUFFER_SIZE], path[MAX_BUFFER_SIZE], version[MAX_BUFFER_SIZE];
-    sscanf(buffer, "%s %s %s", method, path, version);
-
-    char *user_agent = get_header_value(strstr(buffer, "\r\n") + 2, "User-Agent");
-    char *request_body = strstr(buffer, "\r\n\r\n");
-    request_body += strlen("\r\n\r\n");
-
-    Response *response = process_request(method, path, user_agent, request_body);
-    send_response(client_fd, response);
-
-    free_response(response);
-    free(user_agent);
-
-    close(client_fd);
-    return NULL;
-}
-
-int main(int argv, char **argc) {
+int main(int argc, char **argv) {
     setbuf(stdout, NULL);
     printf("Logs from your program will appear here!\n");
 
@@ -225,7 +330,7 @@ int main(int argv, char **argc) {
     struct option long_options[] = {
         {"directory", required_argument, NULL, 'd'},
         {NULL, 0, NULL, 0}};
-    while ((opt = getopt_long(argv, argc, "d:", long_options, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "d:", long_options, NULL)) != -1) {
         switch (opt) {
         case 'd':
             dir_path = optarg;
@@ -248,7 +353,7 @@ int main(int argv, char **argc) {
         pthread_t thread;
         int *arg = malloc(sizeof(int));
         *arg = client_fd;
-        if (pthread_create(&thread, NULL, handle_request, arg) != 0) {
+        if (pthread_create(&thread, NULL, handle_client, arg) != 0) {
             perror("Thread creation failed");
             close(client_fd);
             free(arg);
