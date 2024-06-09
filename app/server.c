dbbced053aab66e32a463b6dd2c27b43f06e40e0
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2024-06-09 16:53:41 +0530

    Added concurrent requests

--- app/server.c
+++ app/server.c
@@ -1,143 +1,189 @@
-#include <errno.h>
+#include <asm-generic/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-int main() {
-    // Disable output buffering
-    setbuf(stdout, NULL);
+#define MAX_BUFFER_SIZE 4096
+#define MAX_HEADER_SIZE 1024
+#define SERVER_PORT 4221
+#define BACKLOG 1
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    printf("Logs from your program will appear here!\n");
+typedef struct {
+    int status_code;
+    const char *reason_phrase;
+    const char *content_type;
+    char *body;
+} Response;
 
-    // Uncomment this block to pass the first stage
-
-    socklen_t server_fd, client_addr_len;
-    struct sockaddr_in client_addr;
-
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+int create_server_socket() {
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) {
-        printf("Socket creation failed: %s...\n", strerror(errno));
-        return 1;
+        perror("Socket creation failed");
+        exit(1);
     }
 
-    // Since the tester restarts your program quite often, setting REUSE_PORT
-    // ensures that we don't run into 'Address already in use' errors
     int reuse = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-        printf("SO_REUSEPORT failed: %s \n", strerror(errno));
-        return 1;
+        perror("SO_REUSEPORT failed");
+        exit(1);
     }
 
     struct sockaddr_in serv_addr = {
         .sin_family = AF_INET,
-        .sin_port = htons(4221),
+        .sin_port = htons(SERVER_PORT),
         .sin_addr = {htonl(INADDR_ANY)},
     };
 
     if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
-        printf("Bind failed: %s \n", strerror(errno));
-        return 1;
+        perror("Bind failed");
+        exit(1);
     }
 
-    int connection_backlog = 5;
-    if (listen(server_fd, connection_backlog) != 0) {
-        printf("Listen failed: %s \n", strerror(errno));
-        return 1;
+    if (listen(server_fd, BACKLOG) != 0) {
+        perror("Listen failed");
+        exit(1);
     }
 
-    printf("Waiting for a client to connect...\n");
-    client_addr_len = sizeof(client_addr);
+    return server_fd;
+}
 
-    int fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-    printf("Client connected\n");
-
-    char buffer[4096];
-    ssize_t byte_read = read(fd, buffer, sizeof(buffer) - 1);
-    if (byte_read < 0) {
-        printf("Read failed: %s\n", strerror(errno));
-        close(fd);
-        close(server_fd);
-        return 1;
-    }
-    buffer[byte_read] = '\0';
-
-    printf("Received request:\n %s\n", buffer);
-
-    char method[8], path[1024], version[16];
-    sscanf(buffer, "%s %s %s", method, path, version);
-
-    // printf("Buffer: %s\n\n", buffer);
-    printf("Method: %s, Path: %s, Version: %s\n", method, path, version);
-
-    char *header_start = strstr(buffer, "\r\n") + 2;
+char *get_header_value(const char *header_start, const char *header_name) {
     char *header_end;
-    char *user_agent_header = NULL;
     while ((header_end = strstr(header_start, "\r\n"))) {
         if (header_start == header_end) {
             break;
         }
 
-        char header_name[256], header_value[256];
-        sscanf(header_start, "%[^:]: %[^\r\n]", header_name, header_value);
-        // printf("Header - %s: %s\n", header_name, header_value);
-        if (strcmp(header_name, "User-Agent") == 0) {
-            user_agent_header = malloc(strlen(header_value) + 1);
-            if (user_agent_header != NULL) {
-                strcpy(user_agent_header, header_value);
-            }
-        }
-
+        char header[MAX_HEADER_SIZE];
+        sscanf(header_start, "%[^\r\n]", header);
         header_start = header_end + 2;
-    }
 
-    int http_status_code;
-    const char *reason_phrase;
-    const char *content_type = "text/plain";
-    char *response_body = NULL;
+        char name[MAX_HEADER_SIZE], value[MAX_HEADER_SIZE];
+        if (sscanf(header, "%[^:]: %[^\r\n]", name, value) == 2 && strcmp(name, header_name) == 0) {
+            return strdup(value);
+        }
+    }
+    return NULL;
+}
+
+Response *create_response(int status_code, const char *reason_phrase, const char *body) {
+    Response *response = malloc(sizeof(Response));
+    response->status_code = status_code;
+    response->reason_phrase = reason_phrase;
+    response->content_type = "text/plain";
+    response->body = strdup(body);
+    return response;
+}
+
+Response *process_request(const char *method, const char *path, const char *user_agent) {
     if (strcmp(path, "/") == 0) {
-        http_status_code = 200;
-        reason_phrase = "OK";
-        response_body = "";
+        return create_response(200, "OK", "");
     } else if (strncmp(path, "/echo/", 6) == 0) {
-        http_status_code = 200;
-        reason_phrase = "OK";
-        size_t response_lenth = strlen(path + 6) + 1;
-        response_body = malloc(response_lenth);
-        if (response_body != NULL) {
-            strcpy(response_body, path + 6);
-        }
+        return create_response(200, "OK", path + 6);
     } else if (strcmp(path, "/user-agent") == 0) {
-        http_status_code = 200;
-        reason_phrase = "OK";
-        size_t response_lenth = strlen(user_agent_header) + 1;
-        response_body = malloc(response_lenth);
-        if (response_body != NULL) {
-            strcpy(response_body, user_agent_header);
-        }
-
+        return create_response(200, "OK", user_agent ? user_agent : "");
     } else {
-        http_status_code = 404;
-        reason_phrase = "Not Found";
-        response_body = "";
+        return create_response(404, "Not Found", "");
     }
-    char headers[1024];
+}
+
+void send_response(int fd, Response *response) {
+    char headers[MAX_HEADER_SIZE];
     snprintf(headers, sizeof(headers),
              "HTTP/1.1 %d %s\r\n"
              "Content-Type: %s\r\n"
              "Content-Length: %zu\r\n"
+             "Connection: keep-alive\r\n"
              "\r\n",
-             http_status_code, reason_phrase, content_type, strlen(response_body));
-    char response[4096];
-    snprintf(response, sizeof(response), "%s%s", headers, response_body);
-    int byte_sent = send(fd, response, strlen(response), 0);
-    if (byte_sent < 0) {
-        printf("Send failed: %s\n", strerror(errno));
-        return 1;
+             response->status_code, response->reason_phrase, response->content_type, strlen(response->body));
+
+    ssize_t headers_len = strlen(headers);
+    ssize_t body_len = strlen(response->body);
+
+    ssize_t total_len = headers_len + body_len;
+    char *response_buffer = malloc(total_len);
+    if (response_buffer == NULL) {
+        perror("Memory allocation failed");
+        return;
+    }
+
+    memcpy(response_buffer, headers, headers_len);
+    memcpy(response_buffer + headers_len, response->body, body_len);
+
+    ssize_t bytes_sent = send(fd, response_buffer, total_len, 0);
+    if (bytes_sent < 0) {
+        perror("Send failed");
+    }
+
+    free(response_buffer);
+}
+
+void free_response(Response *response) {
+    free(response->body);
+    free(response);
+}
+
+void *handle_request(void *arg) {
+    int client_fd = *(int *)arg;
+    free(arg);
+
+    char buffer[MAX_BUFFER_SIZE];
+    ssize_t byte_read = read(client_fd, buffer, sizeof(buffer) - 1);
+    if (byte_read <= 0) {
+        if (byte_read < 0) {
+            perror("Read failed");
+        }
+        close(client_fd);
+        return NULL;
+    }
+    buffer[byte_read] = '\0';
+
+    char method[MAX_BUFFER_SIZE], path[MAX_BUFFER_SIZE], version[MAX_BUFFER_SIZE];
+    sscanf(buffer, "%s %s %s", method, path, version);
+
+    char *user_agent = get_header_value(strstr(buffer, "\r\n") + 2, "User-Agent");
+
+    Response *response = process_request(method, path, user_agent);
+    send_response(client_fd, response);
+
+    free_response(response);
+    free(user_agent);
+
+    close(client_fd);
+    return NULL;
+}
+
+int main() {
+    setbuf(stdout, NULL);
+    printf("Logs from your program will appear here!\n");
+
+    int server_fd = create_server_socket();
+    printf("Waiting for a client to connect...\n");
+
+    while (1) {
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_fd < 0) {
+            perror("Accept failed");
+            continue;
+        }
+
+        pthread_t thread;
+        int *arg = malloc(sizeof(int));
+        *arg = client_fd;
+        if (pthread_create(&thread, NULL, handle_request, arg) != 0) {
+            perror("Thread creation failed");
+            close(client_fd);
+            free(arg);
+        } else {
+            pthread_detach(thread);
+        }
     }
 
     close(server_fd);
